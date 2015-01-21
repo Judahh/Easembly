daffd6449d63af049a7a235214917ba6db0344db
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2015-01-21 19:19:12 -0300

    update

--- Website/Easembly/View/Frames/Common/Common/Common/Body/Common.php
+++ Website/Easembly/View/Frames/Common/Common/Common/Body/Common.php
@@ -1,5 +1,11 @@
 <main>
-    <?php
-        include 'View/Frames/Common/Common/Window/Common/Code/Common.php';
-    ?>
+    <div id="DivIdTable">
+        <div id="DivIdTableRow">
+            <div id="DivIdTableRowWrap">
+                <?php
+                    include 'View/Frames/Common/Common/Window/Common/Code/Common.php';
+                ?>
+            </div>
+        </div>
+    </div>
 </main>
