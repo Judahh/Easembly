359ed608db48434a6357cd38ad7be6ffcbe03e51
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2015-10-02 11:20:32 -0300

    update

--- Website/Easembly/View/Frames/Common/Common/Common/Header/MenuLeft.php
+++ Website/Easembly/View/Frames/Common/Common/Common/Header/MenuLeft.php
@@ -1,11 +1,11 @@
-<div id="DivIdSomeOpacity">
-    <div id="DivIdMenuLeft">
-        <ul>
-            <li>
-                <div id="DivIdMenuItem">
-                    <div id="DivIdBasicIcon" style="color: #000000"><div id="DivIdPulse">S</div></div>
-                </div>
-            </li>
-        </ul>
-    </div>
-</div>
+<!--<div id="DivIdSomeOpacity">-->
+<!--    <div id="DivIdMenuLeft">-->
+<!--        <ul>-->
+<!--            <li>-->
+<!--                <div id="DivIdMenuItem">-->
+<!--                    <div id="DivIdBasicIcon" style="color: #000000"><div id="DivIdPulse">S</div></div>-->
+<!--                </div>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--    </div>-->
+<!--</div>-->
